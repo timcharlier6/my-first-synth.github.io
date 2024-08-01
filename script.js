78aede0b9e09b412aeab0d377c7ddefc735a4a9a
Tim Charlier <timcharlier6@etik.com>
2024-08-01 14:49:11 +0200

    update

--- script.js
+++ script.js
@@ -11,91 +11,62 @@ function playNote(note, name) {
   let square = name;
   switch (note.id) {
     case "c3":
-      synth.triggerAttackRelease("C3", "4n"); 
-      square.textContent="Do";
+      synth.triggerAttackRelease("C3", "4n");
+      square.textContent = "Do";
       break;
     case "d3":
       synth.triggerAttackRelease("D3", "4n");
-      square.textContent="Ré";
+      square.textContent = "Ré";
       break;
     case "e3":
       synth.triggerAttackRelease("E3", "4n");
-      square.textContent="Mi";
+      square.textContent = "Mi";
       break;
     case "f3":
       synth.triggerAttackRelease("F3", "4n");
-      square.textContent="Fa";
+      square.textContent = "Fa";
       break;
     case "g3":
       synth.triggerAttackRelease("G3", "4n");
-      square.textContent="Sol";
+      square.textContent = "Sol";
       break;
     case "a3":
       synth.triggerAttackRelease("A3", "4n");
-      square.textContent="La";
+      square.textContent = "La";
       break;
     case "b3":
       synth.triggerAttackRelease("B3", "4n");
-      square.textContent="Si";
+      square.textContent = "Si";
       break;
     case "c4":
       synth.triggerAttackRelease("C4", "4n");
-      square.textContent="Do";
+      square.textContent = "Do";
       break;
     case "d4":
       synth.triggerAttackRelease("D4", "4n");
-      square.textContent="Ré";
+      square.textContent = "Ré";
       break;
     case "e4":
       synth.triggerAttackRelease("E4", "4n");
-      square.textContent="Mi";
+      square.textContent = "Mi";
       break;
     case "f4":
       synth.triggerAttackRelease("F4", "4n");
-      square.textContent="Fa";
+      square.textContent = "Fa";
       break;
     case "g4":
       synth.triggerAttackRelease("G4", "4n");
-      square.textContent="Sol";
+      square.textContent = "Sol";
       break;
-     case "a4":
+    case "a4":
       synth.triggerAttackRelease("A4", "4n");
-      square.textContent="La";
-      break; 
-     case "b4":
+      square.textContent = "La";
+      break;
+    case "b4":
       synth.triggerAttackRelease("B4", "4n");
-      square.textContent="Si";
+      square.textContent = "Si";
       break;
-    case "c5":
-      synth.triggerAttackRelease("C5", "4n");
-      square.textContent="Do";
-      break;
-    case "d5":
-      synth.triggerAttackRelease("D5", "4n");
-      square.textContent="Ré";
-      break;
-    case "e5":
-      synth.triggerAttackRelease("E5", "4n");
-      square.textContent="Mi";
-      break;
-    case "f5":
-      synth.triggerAttackRelease("F5", "4n");
-      square.textContent="Fa";
-      break;
-    case "g5":
-      synth.triggerAttackRelease("G5", "4n");
-      square.textContent="Sol";
-      break;
-    case "a5":
-      synth.triggerAttackRelease("A5", "4n");
-      square.textContent="La";
-      break;
-     case "b5":
-      synth.triggerAttackRelease("B5", "4n");
-      square.textContent="Si";
-      break;  
   }
-  
 }
 
 const keys = document.getElementsByClassName("key");
@@ -106,7 +77,7 @@ for (let i = 0; i < keys.length; i++) {
     if (Tone.context.state !== "running") {
       Tone.start();
     }
-    playNote(this, square); 
+    playNote(this, square);
     changeColorOnClick(this);
   });
 }
